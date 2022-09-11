01da6034fd91227f1d20e3aa5135ba827be03c08
Jack Waters <61799247+ggtwig@users.noreply.github.com>
2022-09-11 17:01:42 +0100

    registered SetHome and Home commands

--- src/main/java/com/jack/functionality/Main.java
+++ src/main/java/com/jack/functionality/Main.java
@@ -1,5 +1,8 @@
 package com.jack.functionality;
 
+import com.jack.functionality.commands.HomeCommand;
+import com.jack.functionality.commands.SetHomeCommand;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
@@ -12,6 +15,8 @@ public final class Main extends JavaPlugin {
 
     private void registerCommands() {
         //getCommand("").setExecutor(...);
+        getCommand("sethome").setExecutor(new SetHomeCommand());
+        getCommand("home").setExecutor(new HomeCommand());
     }
 
 }
--- src/main/java/com/jack/functionality/commands/HomeCommand.java
+++ src/main/java/com/jack/functionality/commands/HomeCommand.java
@@ -1,4 +1,21 @@
 package com.jack.functionality.commands;
 
-public class HomeCommand {
+import com.jack.functionality.utils.ChatUtils;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class HomeCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (!(sender instanceof Player)) {
+            ChatUtils.notPlayerErrorMessage(sender);
+            return true;
+        }
+
+        return true;
+    }
 }
